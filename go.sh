962ee44aa086d7f95435369f576e28184bba4795
Alvaro Cavalcanti <alvarovictor@gmail.com>
2014-09-18 14:46:52 -0300

    add rss to cms importer

--- go.sh
+++ go.sh
@@ -25,6 +25,10 @@ if [ ! -e "$REPOSITORIES_FOLDER/semantic-repository" ]; then
   git clone git@github.com:W3CBrasil/od4d-semantic-repository.git semantic-repository
 fi
 
+if [ ! -e "$REPOSITORIES_FOLDER/rss-to-cms" ]; then
+  git clone https://github.com/W3CBrasil/od4d-rss-to-cms.git rss-to-cms
+fi
+
 popd &> /dev/null
 
 echo "Done dowloading repositories."
